fea6563b472a2b774010157c7164b1e84b646ff6
Olivia Anderson <liv.and1012@gmail.com>
2023-06-06 15:03:28 -0400

    user feed

--- src/FeedComponent/TLFeed.js
+++ src/FeedComponent/TLFeed.js
@@ -1,5 +1,16 @@
+import React from "react"
+
+
 const tLFeed =() =>{
     return(
-        <div
+        <div className="userProfile">
+            <div className="user-container">
+                <div className="user-post">
+
+                </div>
+            </div>
+        </div>
     )
 }
+
+export default tLFeed
