8f1a8f5ee6655d99a51b5e510c313af36c7e4137
Olivia Anderson <liv.and1012@gmail.com>
2023-06-07 13:43:44 -0400

    addind changes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 import Main from "./components/Main";
 import SignIn from "./components/LoginSignIn/SignIn";
+import SideBar from "./components/Sidebar/Sidebar";
 function App() {
     return (
         <div className="App">
+            <SideBar />
             <SignIn/>
         </div>
     );
