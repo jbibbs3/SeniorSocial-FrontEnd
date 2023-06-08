a7e8c73fa3a1b8a3cd959fd7394de4ea1503f715
Olivia Anderson <liv.and1012@gmail.com>
2023-06-08 11:14:43 -0400

    adding post box

--- src/FeedComponent/CreatePost.js
+++ src/FeedComponent/CreatePost.js
@@ -1,11 +1,34 @@
-import { display } from "@mui/system"
+import { border, display, padding } from "@mui/system"
 
 const CreatePost =()=>{
    const styles={
-        header: {
-            fontWeight:"bold",
-            color:"blue"
-            display:
+
+        postBox:{
+            background:"#e4e7eb",
+            borderRadius:"15px",
+            maxWidth:"475px",
+            width:"100%",
+            padding: "25px 25px 15px 25px"
+        },
+
+        postInput:{
+            paddingTop:"10px",
+            borderBottom:"1px solid #e4e7eb"
+        },
+
+        textArea:{
+            minHeight:"230px"
+        },
+
+        placeholder:{
+            marginLeft: "-7cm",
+            marginTop:"3px",
+            fontSize:"22px",
+            color:"#98a5b1",
+            pointerEvents:"none"
+        },
+        inputEdit:{
+            background:""
 
         }
     }
@@ -14,11 +37,11 @@ const CreatePost =()=>{
 
 
     return(
-        <div className="postBox">
-            <div className="postInput">
-                <div className="text-area">
-                    <span className="placeholder" style={styles.header}>What's Going On</span>
-                    <div className="input-edit" contentEditable="true" spellCheck="false"></div>
+        <div className="postBox" style={styles.postBox}>
+            <div className="postInput" style={styles.postInput}>
+                <div className="text-area" style={styles.textArea}>
+                    <span className="placeholder" style={styles.placeholder}>What's Going On</span>
+                    <div className="input-edit" contentEditable="true" spellCheck="false" style={styles.inputEdit}></div>
                     <div className="input-read" contentEditable="true" spellCheck="false "></div>
                 </div>
                 <div className="b-half">
