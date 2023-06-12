62f2d182fb36e3ee06770e2f9af6ee96c35cdf24
Nati Worku <natiworku1971@gmail.com>
2023-06-12 15:31:26 -0400

    post render

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import UserPosts from "../FeedComponent/UserPost";
 import Sidebar from "./Sidebar/Sidebar";
 import User from "./UserProfile/User";
 import FeedComponent from "../FeedComponent/FeedComponent";
+import SocialContext from "../SocialContext";
 const Main = () => {
+    const { user, setUser, posts, setPosts, savedPosts, setSavedPosts, myPosts, setMyPosts } =
+        useContext(SocialContext);
+    useEffect(() => {
+        fetch("http://localhost:3000/posts")
+            .then((res) => res.json())
+            .then((fetchPosts) => setPosts([...posts, ...fetchPosts]));
+            console.log([...posts]);
+    },[]);
     return (
         <div>
             <Sidebar></Sidebar>
@@ -14,7 +23,7 @@ const Main = () => {
                 <Route>Message</Route>
                 <Route>Profile</Route>
             </Routes> */}
-            <FeedComponent/>
+            <FeedComponent />
         </div>
     );
 };
