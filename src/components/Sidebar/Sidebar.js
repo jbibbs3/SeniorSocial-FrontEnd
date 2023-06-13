e41d0a0e254ea4ab9aa48a4b210e85bcc6b61ded
COR Coding <corcoding@Michaels-iPhone.localdomain>
2023-06-13 10:17:12 -0400

    updated logo

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -6,7 +6,7 @@ import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import EmailIcon from "@mui/icons-material/Email";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-import logo from "./New-senior-social-logo.jpg";
+import logo from "./Logo with Tag (1).jpg";
 import UserPosts from "../../FeedComponent/UserPost";
 const SideBar = () => {
     const [collapsed, setCollapsed] = useState(false);
